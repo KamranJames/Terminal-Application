cdb418d84eeedacf11c44db25bd0f8304870fc0d
Kamran Fulleylove <kamran-j-fulleylove@Kamrans-MacBook-Pro.local>
2022-09-22 17:27:17 +1000

    Remote was in wrong dir

---
+++ main.py
@@ -0,0 +1,80 @@
+import cowsay 
+from colorama import init
+from termcolor import colored
+from modules import main
+init()
+
+# from pick import pick
+
+guesses = 0
+print("Welcome to the guessing game you have 3 guesses your first riddle is: \n")
+
+while True and guesses < 3: 
+    print("I am cool as a breeze, I stop your heart with ease, seasons may be my enemy, but im part of you and you are friend to me. What am I? \n")
+    print("Enter your guess or type 'hint', or 'quit' below.")
+    answer = input()
+    answer = answer.lower()
+    guesses +=1
+
+    if "sneeze" in answer.lower():
+       cowsay.cow(colored("Great work, your next riddle is: \n,",'green', 'on_cyan'))
+
+       break
+
+    elif answer == "hint": 
+       guesses -=1
+       cowsay.cow(colored("Achoo!\n",'green', 'on_cyan'))
+        
+    
+    elif answer == "quit" or guesses == 3:
+           print(colored("Better luck next time! ", "green", 'on_cyan'))
+           quit()
+
+    else:
+        print("Try again!",'green', 'on_cyan')
+
+
+while True and guesses < 3: 
+    print("\n I am fast or I am slow, i'll keep you young and on your toes, your in charge of my tempo. What am I? \n")
+    print("Enter your guess or type 'hint', or 'quit' below.")
+    answer = input()
+    answer = answer.lower()
+    guesses +=1
+
+    if "running" in answer:
+       cowsay.cow("\n Great work, your next riddle is: \n",'green', 'on_cyan')
+       break
+
+    elif answer == "hint": 
+       guesses -=1
+       cowsay.cow("A way to go places.\n",'green', 'on_cyan')
+    
+    elif answer == "quit" or guesses == 3:
+           cowsay.cow("Better luck next time! ",'green', 'on_cyan')
+           quit()
+
+    else:
+        print("Try again!",'green', 'on_cyan')
+
+
+while True and guesses < 3: 
+    print("Pearl white chest without key or lid. Inside of which, a golden treasure is hid. What am I?  \n",'green', 'on_cyan')
+    print("Enter your guess or type 'hint', or 'quit' below.",'green', 'on_cyan')
+    answer = input()
+    answer = answer.lower()
+    guesses +=1
+
+    if "egg" in answer:
+       cowsay.cow("\n CONGRATULATIONS, YOU ARE THE RIDDLE MASTER",'green', 'on_cyan')
+       break
+
+    elif answer == "hint":
+       guesses -=1
+       cowsay.cow("The creator of this chest is a farm animal\n",'green', 'on_cyan')
+    
+    elif answer == "quit" or guesses == 3:
+        cowsay.cow("Better luck next time! ",'green', 'on_cyan')
+        quit()
+
+    else:
+        print("Try again!",'green', 'on_cyan')
---
+++ testmodules.py
@@ -0,0 +1,43 @@
+from simple_term_menu import TerminalMenu
+
+class Guess_Counter:
+  def __init__(self, guessnum):
+   self.guessnum = guessnum
+
+guess = Guess_Counter
+guess.guessnum = 0 
+
+def main():
+    print("Welcome to Riddle me this!! select your difficulty level. The higher difficulty you choose the less guesses you get. \n")
+    main_menu_items = ["Easy = 4 guesses", "Medium = 3 guesses ", "Hard = 2 guesses",
+                     "Quit"]
+    terminal_menu = TerminalMenu(main_menu_items)
+    menu_entry_index = terminal_menu
+
+    main_menu = TerminalMenu(
+        menu_entries=main_menu_items,
+      
+    )
+    
+    while True:
+        main_sel = main_menu.show()
+
+        if main_sel == 0:
+            guess.guessnum == 1
+            break
+            
+        elif main_sel == 1:
+            guess.guessnum == 2
+            break
+            
+        elif main_sel == 2:
+            guess.guessnum +=3
+            break
+           
+        elif main_sel == 3:
+            main_menu_exit = True
+            quit()
+
+
+if __name__ == "__main__":
+    main()
